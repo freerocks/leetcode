40e9095c352ffe0253415142053bd7bd6a20b570
freerocks <destinymobo@gmail.com>
2021-06-30 03:04:43 +0800

    python: 035 solution2

--- python/035_search_insert_position.py
+++ python/035_search_insert_position.py
@@ -8,3 +8,19 @@ class Solution:
                 return min
             min += 1
         return max + 1
+    # solution 2: O(logn) 44ms, 15.4MB
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        min, max = 0, len(nums) - 1
+        mid = 0
+
+        while min <= max :
+            mid = int((min + max) / 2)
+            if target == nums[mid]:
+                return mid
+            elif target < nums[mid]:
+                max = mid - 1
+            else:
+                min = mid + 1
+        if  min > mid :
+            return mid + 1
+        return mid
